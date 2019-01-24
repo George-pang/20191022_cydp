2bf95c50766684b1d93e3781867e60508cf2fa1a
PLQ <714303222@qq.com>
2019-01-24 17:42:03 +0800

    20190124

--- js/pages.js
+++ js/pages.js
@@ -151,32 +151,40 @@ $(function () {
     /* 2、站点布局 展示页 */
     (function () {
         if ($(".site_main").length != 0) {
-            // 本地模拟数据
+            // 本地模拟数据--二维对象数组嵌套
             var data_info = [{
-                "X坐标": 116.456937,
-                "Y坐标": 39.93331,
-                "title": "朝阳1分站",
-                "content": "这是朝阳120第一分站",
-                "phone": 87789999,
-            }, {
-                "X坐标": 116.4457321,
-                "Y坐标": 39.92469024,
-                "title": "朝阳2分站",
-                "content": "这是朝阳120第二分站",
-                "phone": 87789999,
-            }, {
-                "X坐标": 116.451992,
-                "Y坐标": 39.932234,
-                "title": "朝阳3分站",
-                "content": "这是朝阳120第三分站",
-                "phone": 87789999
-            }, {
-                "X坐标": 116.481685,
-                "Y坐标": 39.938963,
-                "title": "朝阳4分站",
-                "content": "这是朝阳120第四分站",
-                "phone": 87789999
-            }];
+                    name: "朝阳中心",
+                    stationList: [{
+                        "X坐标": 116.456937,
+                        "Y坐标": 39.93331,
+                        "name": "朝阳中心1分站",
+                        "content": "这是朝阳中心第一分站",
+                        "phone": 87789999,
+                    }, {
+                        "X坐标": 116.4457321,
+                        "Y坐标": 39.92469024,
+                        "name": "朝阳中心2分站",
+                        "content": "这是朝阳中心第二分站",
+                        "phone": 87789999,
+                    }]
+                },
+                {
+                    name: "垂杨柳分中心",
+                    stationList: [{
+                        "X坐标": 116.451992,
+                        "Y坐标": 39.932234,
+                        "name": "垂杨柳中心1分站",
+                        "content": "这是垂杨柳中心第一分站",
+                        "phone": 87789999,
+                    }, {
+                        "X坐标": 116.481685,
+                        "Y坐标": 39.938963,
+                        "name": "垂杨柳中心2分站",
+                        "content": "这是垂杨柳中心第二分站",
+                        "phone": 87789999,
+                    }]
+                }
+            ];
             //信息窗口通用配置项
             var opts = {
                 width: 0, // 信息窗口宽度
@@ -185,34 +193,24 @@ $(function () {
                 // title: data_info[i].title, // 信息窗口标题
                 enableMessage: true //设置允许信息窗发送短息
             };
+
             //创建地图实例
             var map = new BMap.Map("mapContainer");
-            mapInit();
-            // getBoundary();
-
+            mapInit(); //初始化地图
+            getBoundary(); //显示朝阳区行政版块覆盖物
             //创建多个标注点并添加信息窗口
-            for (var i = 0; i < data_info.length; i++) {
-                var point = new BMap.Point(data_info[i].X坐标, data_info[i].Y坐标); //创建标注点
-                // 自定义信息窗口标题
-                // opts.title = data_info[i].title;
-                var marker = new BMap.Marker(point); // 创建标注
-                // var content = data_info[i].content;
-                var content = '<h4 style= "font-size:18px;line-height:24px;margin-bottom:5px;color:blue">' +
-                    data_info[i].title + '</h4><p style="line-height:22px">' +
-                    '联系电话：' + data_info[i].phone + '<br/>' +
-                    '坐标：' + data_info[i].X坐标 + ',' + data_info[i].Y坐标;
-                map.addOverlay(marker);
-                addClickHandler(content, marker);
-            }
-            //渲染右侧地图标注列表
-            renderMarkerList(data_info);
-            //注册点击事件
-            $("#markerList").on("click", "li", liElementClickHandler);
+            renderPoint(data_info, "stationList");
+            //渲染地图右侧信息块 内容
+            renderCenterList(data_info);
+            //分站列表的折叠展开
+            $(".centerList").on("click", ".centerLink", centerItemClickHandler);
+            //给每个分站项注册点击事件
+            $(".stationList").on("click", "li", stationItemClickHandler);
 
 
             //function：初始化地图配置
             function mapInit() {
-                map.centerAndZoom('北京市朝阳区', 15); //初始化地图中心点及缩放级别
+                map.centerAndZoom('北京市朝阳区'); //初始化地图中心点及缩放级别，当中心点为字符串时,缩放级别可省略，自动适配
                 // 百度地图API功能
                 map.enableScrollWheelZoom(); //启用滚轮放大缩小
                 // map.disableDragging();//禁用地图拖拽
@@ -229,8 +227,34 @@ $(function () {
                 map.addControl(navigationControl);
 
             }
+            //function:根据传入配置项对象来设置信息窗口的内容
+            function setInfoWindowContent(obj) {
+                var content = '<h4 style= "font-size:18px;line-height:24px;margin-bottom:5px;color:blue">' +
+                    obj.name + '</h4><p style="line-height:22px">' +
+                    '联系电话：' + obj.phone + '<br/>' +
+                    '坐标：' + obj.X坐标 + ',' + obj.Y坐标;
+                return content;
 
-            //function:地图标注点点击处理函数
+            }
+            //function:遍历数据，渲染标注点:参数1:数据;参数2：数据内嵌套的分站数据数组名
+            function renderPoint(data, stationDataName) {
+                for (let i = 0; i < data.length; i++) {
+                    for (let j = 0; j < data[i][stationDataName].length; j++) {
+                        var point = new BMap.Point(data[i][stationDataName][j].X坐标, data[i][stationDataName][j].Y坐标); //创建标注点
+                        // 自定义信息窗口标题
+                        // opts.title = data_info[i].title;
+                        var marker = new BMap.Marker(point); // 创建标注
+                        // marker.setTop(true);//将标注置于其他标注之上
+                        // marker.setZIndex(999);
+                        map.addOverlay(marker);
+                        marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+                        // 设置标注点信息窗口的内容
+                        var content = setInfoWindowContent(data[i][stationDataName][j]);
+                        addClickHandler(content, marker);
+                    }
+                }
+            };
+            //function:地图标注点的点击事件处理函数
             function addClickHandler(content, marker) {
                 marker.addEventListener("click", function (e) {
                     openInfo(content, e)
@@ -243,38 +267,76 @@ $(function () {
                 var infoWindow = new BMap.InfoWindow(content, opts); // 创建信息窗口对象 
                 map.openInfoWindow(infoWindow, point); //开启信息窗口
             }
-            // function:右侧地图信息列表渲染
-            function renderMarkerList(data) {
+            // function:右侧地图信息块 列表渲染中心列表
+            function renderCenterList(data) {
                 for (var i = 0; i < data.length; i++) {
-                    str = '<li class="markerItem" id="no0"> ' +
-                        '<span class="icon" id="mk_0" style="background-image:url(images/marker.png)"></span>' +
-                        '<div id="no_0" class="txt">' +
-                        '<a href="javascript:void(0)" title="' + data[i].title + '" class="aLink title">' + data[i].title +
-                        '</a><p class="info" data-x="' + data[i].X坐标 + '" data-y="' + data[i].Y坐标 + '"data-content="' + data[i].content +
+                    var centerItemStr = '<li class="centerItem">' +
+                        '<a href="javascript:;" class="centerLink">' +
+                        '<i class="centerIcon" style="background-image:url(images/center.png)"></i>' +
+                        '<span class="centerName">' + data[i].name + '</span>' +
+                        '<i class="arrow" style="background-image:url(images/arrow_up.png)"></i>' +
+                        '</a></li>';
+                    var stationListStr = '<ul class="stationList"></ul>';
+                    var centerItemObj = $(centerItemStr); //new centerItemObj（li）
+                    var stationListObj = $(stationListStr); //new stationListObj（ul）
+                    //列表渲染分站列表
+                    renderStationList(data_info[i].stationList, stationListObj)
+                    //插入到DOM中
+                    centerItemObj.append(stationListObj);
+                    $(".centerList").append(centerItemObj);
+                    // renderStationList(data_info[i].stationList);
+                }
+
+            }
+            // function:右侧地图信息块 列表渲染分站列表并给每一个分站注册点击事件
+            function renderStationList(data, obj) {
+                for (var i = 0; i < data.length; i++) {
+                    var stationItemstr = '<li class="stationItem"> ' +
+                        '<span class="icon" style="background-image:url(images/marker.png)"></span>' +
+                        '<div class="txt">' +
+                        '<a href="javascript:void(0)" title="' + data[i].name + '" class="aLink">' + data[i].name +
+                        '</a><p class="info" data-x="' + data[i].X坐标 + '" data-y="' + data[i].Y坐标 + '"data-content="' +
+                        data[i].content +
                         '">地址：' +
                         data[i].content + '<br>电话：' + data[i].phone + '<br>坐标：' + data[i].X坐标 + ',' + data[i].Y坐标 +
                         '</p></div></li>';
-                    $("#markerList").append(str); //动态插入li标签
+                    var stationItemObj = $(stationItemstr);
+                    obj.append(stationItemObj);
+                    //将右侧每个分站的信息窗内容存到li元素的自定义属性上，方便点击事件时获取对应的内容
+                    content = setInfoWindowContent(data[i]);
+                    stationItemObj.attr("data-content", content);
                 }
-            }
 
-            function liElementClickHandler() {
+            }
+            //function:分站列表每个li元素的点击事件处理函数
+            function stationItemClickHandler() {
+                var content = $(this).attr("data-content");
                 var x = $(this).find("p").attr("data-x");
                 var y = $(this).find("p").attr("data-y");
                 var point = new BMap.Point(x, y);
                 // 将地图的中心点更改为给定的点。
                 map.panTo(point);
-                // var content = $(this).find("p").attr("data-content");
                 var infoWindow = new BMap.InfoWindow(content, opts); // 创建信息窗口对象 
                 //打开信息窗
                 map.openInfoWindow(infoWindow, point);
+            }
+            //function: 右侧地图信息块 分站列表的折叠展开
+            function centerItemClickHandler() {
+                $(this).siblings(".stationList").toggle("normal", function () {
+                    // 箭头icon的对应切换
+                    if ($(this).css("display") == "none") {
+                        $(this).siblings(".centerLink").find(".arrow").css("backgroundImage", "url(images/arrow.png)");
+                    } else if ($(this).css("display") == "block") {
+                        $(this).siblings(".centerLink").find(".arrow").css("backgroundImage", "url(images/arrow_up.png)");
+                    }
+                });
             }
             //function:在地图上显示行政区域划分
             function getBoundary() {
                 var bdary = new BMap.Boundary(); //此类表示一个行政区域的边界。
                 //返回行政区域的边界。 name: 查询省、直辖市、地级市、或县的名称。 callback:执行查询后，数据返回到客户端的回调函数
                 bdary.get("北京市朝阳区", function (rs) { //获取行政区域 
-                    map.clearOverlays(); //清除地图覆盖物       
+                    // map.clearOverlays(); //清除地图覆盖物       
                     var count = rs.boundaries.length; //行政区域的点有多少个
                     if (count === 0) {
                         alert('未能获取当前输入行政区域');
@@ -284,17 +346,19 @@ $(function () {
                     for (var i = 0; i < count; i++) {
                         var ply = new BMap.Polygon(rs.boundaries[i], {
                             strokeWeight: 2,
-                            strokeColor: "#ff0000",
-                            fillColor: "#fff",
-                            fillOpacity: 0
+                            strokeOpacity: 0.8,
+                            StrokeStyle: "solid",
+                            strokeColor: "#1abc9c",
+                            fillColor: "#16a085",
+                            fillOpacity: 0.1
                         }); //建立多边形覆盖物
                         map.addOverlay(ply); //添加覆盖物
                         pointArray = pointArray.concat(ply.getPath()); //返回多边型的点数组
                     }
                     //根据提供的地理区域或坐标设置地图视野，调整后的视野会保证包含提供的地理区域或坐标
                     map.setViewport(pointArray); //调整视野  
-                    // map.centerAndZoom('北京市朝阳区', 14);
-                    map.setZoom(15); //将视图切换到指定的缩放等级，中心点坐标不变。
+                    map.centerAndZoom('北京市朝阳区'); //设置地图中心点及缩放级别
+                    // map.setZoom(13); //将视图切换到指定的缩放等级，中心点坐标不变。
                 });
             }
         }
@@ -306,10 +370,10 @@ $(function () {
         if ($(".event_main").length != 0) {
             // 本地测试数据
             var data = [{
-                    "area": "朝外街道",
+                    "area": "来广营乡",
                     "num": 10,
-                    "x坐标": 116.489279,
-                    "y坐标": 39.931144,
+                    "x坐标": 116.440564,
+                    "y坐标": 40.038123,
                     "event": "CO中毒"
                 },
                 {
@@ -342,7 +406,7 @@ $(function () {
 
             //function:初始化地图配置
             function eventMapInit() {
-                mp.centerAndZoom("北京市朝阳区", 15);
+                mp.centerAndZoom("北京市朝阳区");
                 mp.enableScrollWheelZoom();
             }
             //自定义构造函数: 复杂的自定义覆盖物
@@ -365,22 +429,22 @@ $(function () {
                     div.style.backgroundColor = this._bgColor;
                     // div.style.border = "1px solid #BC3B3A";
                     div.style.color = "white";
-                    div.style.height = "25px";
-                    div.style.padding = "0 15px";
-                    div.style.lineHeight = "20px";
+                    div.style.height = "40px";
+                    div.style.padding = "0 12px";
+                    div.style.lineHeight = "40px";
                     div.style.whiteSpace = "nowrap";
                     div.style.MozUserSelect = "none";
-                    div.style.fontSize = "16px"
+                    div.style.fontSize = "18px"
                     div.style.borderRadius = "4px";
                     div.style.boxShadow = "1px 2px 3px rgba(0,0,0,0.3)";
                     div.style.cursor = "pointer";
                     var span = this._span = document.createElement("span");
                     span.style.display = "inline-block";
-                    span.style.height = "25px";
-                    span.style.lineHeight = "25px";
+                    span.style.height = "40px";
+                    span.style.lineHeight = "40px";
                     div.appendChild(span);
                     span.appendChild(document.createTextNode(this._text + "\t" + this._num + "个"));
-                    var that = this;
+                    var that = this; //防止与事件处理函数内部的this冲突
 
                     //倒三角箭头
                     var arrow = this._arrow = document.createElement("i");
@@ -392,11 +456,11 @@ $(function () {
                     arrow.style.height = 0;
                     arrow.style.lineHeight = 0;
                     arrow.style.fontSize = 0;
-                    arrow.style.borderWidth = "8px";
+                    arrow.style.borderWidth = "15px 8px";
                     arrow.style.borderStyle = "solid dashed dashed dashed";
                     arrow.style.position = "absolute";
-                    arrow.style.top = "25px";
-                    arrow.style.left = "50%";
+                    arrow.style.top = "40px";
+                    arrow.style.left = "30%";
                     arrow.style.transform = "translateX(-50%)";
                     arrow.style.opacity = .85;
                     div.appendChild(arrow);
@@ -409,7 +473,7 @@ $(function () {
                     }
 
                     div.onmouseout = function () {
-                        console.log(that._bgColor);
+                        // console.log(that._bgColor);
                         this.style.backgroundColor = that._bgColor;
 
                         // arrow.style.backgroundPosition = "0px 0px";
@@ -427,8 +491,9 @@ $(function () {
                     //根据地理坐标获取对应的覆盖物容器的坐标，此方法用于自定义覆盖物
                     var pixel = map.pointToOverlayPixel(this._point);
                     // console.log(pixel); //{x: 450, y: 163}
+                    //div容器的左侧边框坐标=箭头坐标（即点坐标）-箭头相对div左侧的偏移
                     this._div.style.left = pixel.x - parseInt(this._arrow.style.left) + "px";
-                    this._div.style.top = pixel.y - 30 + "px";
+                    this._div.style.top = pixel.y - 40 + "px"; //40为箭头图标相对覆盖物div容器的顶部偏移
                 }
             }
 
